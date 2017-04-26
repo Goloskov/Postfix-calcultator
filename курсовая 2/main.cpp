5015c5e1dc019dc091a1436497b6d19f95ed0cd3
Марк Голосков <superMark@MacBook-Mark.local>
2017-04-26 18:15:07 +0300

    Initial Commit

---
+++ курсовая 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  курсовая 2
+//
+//  Created by Марк Голосков on 26.04.17.
+//  Copyright © 2017 Марк Голосков. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
